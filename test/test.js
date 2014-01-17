273d4a4e9745e8f88abbd6e9904b37579e646bc9
James Halliday <mail@substack.net>
2014-01-17 11:39:07 -0800

    use a dynamic port

--- test/test.js
+++ test/test.js
@@ -5,27 +5,30 @@ var path = require('path');
 var child = require('child_process');
 var testServer = require(path.join(__dirname, 'test-server.js'));
 var repoDir = path.join(__dirname, 'repos');
-var npmOutput = fs.readFileSync(path.join(__dirname, 'npm-install-dat-stdout.txt'));
+var npmOutput = fs.readFileSync(
+    path.join(__dirname, 'npm-install-dat-stdout.txt'), 'utf8'
+);
 
 test('cleanup', cleanup);
 test('setup', setup);
 
 test('all side band messages get received', function(t) {
     var server = testServer();
-    server.listen(5000, push);
+    server.listen(0, push);
     
     function push() {
-        child.exec('git push http://localhost:5000/test.git master', function(err, stderr, stdout) {
+        var port = server.address().port;
+        var cmd = 'git push http://localhost:' + port + '/test.git master';
+        child.exec(cmd, function(err, stderr, stdout) {
             t.false(err);
-
+            
             var lines = [];
-
+            
             stdout.split('\n').map(function(l) {
                 if (l.match(/^remote:/)) lines.push(l);
             });
-
-            var actual = npmOutput.toString().split('\n');
-    
+            var actual = npmOutput.split('\n');
+             
             for (var i = 0; i < lines.length; i++) {
                 t.true(lines[i].indexOf(actual[i]) > -1, 'lines match')
             }
@@ -39,11 +42,12 @@ test('cleanup', cleanup);
 
 function setup(t) {
     fs.mkdirSync(repoDir);
-    child.exec('git init repos/test.git --bare -q', {cwd: __dirname}, function(e, stdout, stderr) {
+    var cmd = 'git init repos/test.git --bare -q';
+    child.exec(cmd, {cwd: __dirname}, function(e, stdout, stderr) {
         t.false(e);
         t.end();
     });
-};
+}
 
 function cleanup(t) {
     rimraf(repoDir, function(err) {
